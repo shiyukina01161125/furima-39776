c672651947828b5558ec091c0f34d1125be0ee36
shiyukina01161125 <akama.techcamp@gmail.com>
2023-09-25 16:08:22 +0900

    テストコードOK

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
+    association :user
+
+    item_name          {'MacbookPro'}
+    item_explanation   {'展示品特価'}
+    category_id        {'2'}
+    item_situation_id  {'2'}
+    delivery_charge_id {'2'}
+    delivery_area_id   {'2'}
+    delivery_day_id    {'2'}
+    price              {'500'}
     
+    after(:build) do |item|
+      item.item_image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
+
