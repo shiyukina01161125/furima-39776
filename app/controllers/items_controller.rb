25b20a9d1ea1aef79e33b6089e768f8c9c3b92ed
shiyukina01161125 <akama.techcamp@gmail.com>
2023-09-30 14:57:27 +0900

    レビュー②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create ]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   
 
   def index
@@ -32,7 +32,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to item_path(@item.id)
     else
       render :edit, status: :unprocessable_entity
     end
