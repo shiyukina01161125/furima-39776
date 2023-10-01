f4a8b087b44ba0ad857e1870003579d7ee63a529
shiyukina01161125 <akama.techcamp@gmail.com>
2023-10-01 17:08:04 +0900

    直接URL遷移禁止OK

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :find_item, only: [:index, :create ]
+  before_action :order_form, only: :index
+
   def index
-  @item = Item.find(params[:item_id])
-  @orderform = OrderForm.new
+    @orderform = OrderForm.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @orderform = OrderForm.new(order_params)
     if @orderform.valid?
       @orderform.save(params,current_user.id)
@@ -14,9 +16,18 @@ class OrdersController < ApplicationController
       render :index
     end
   end
-      
+
   private
   def order_params
     params.require(:order_form).permit(:post_code, :delivery_area_id, :municipality, :street_address, :building_name, :telephone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def order_form
+    return unless current_user != @item.user
+    redirect_to root_path
+  end
+  
+  def find_item
+    @item = Item.find(params[:item_id])
   end
 end
