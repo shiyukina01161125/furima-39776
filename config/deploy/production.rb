bc56eb22f003f48a3fb80a64ee4476b5897837bb
shiyukina01161125 <akama.techcamp@gmail.com>
2023-10-05 18:48:33 +0900

    修正③

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.199.94.189', user: 'ec2-user', roles: %w{app db web}'
+server '52.199.94.189', user: 'ec2-user', roles: %w{app db web}
